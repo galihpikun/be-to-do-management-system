1a62a6e81a0f499ad8363995bdd3aca021fe049b
galihpikun <galihrakha003@gmail.com>
2025-02-22 11:54:26 +0700

    feat:vercel config

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 
+// Import Module & Declare Variable
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Import DB Connection
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var taskRouter = require('./routes/task');
+var userTaskRouter = require('./routes/usertask');
 
+// Create Express App
 var app = express();
 
 // view engine setup
@@ -21,10 +27,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define Route
+// Greeting API
 app.use('/', indexRouter);
+
+// Users API
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Task API
+app.use('/tasks', taskRouter);
+
+// User Task API
+app.use('/usertask', userTaskRouter);
+
+// Handle Error
 app.use(function (req, res, next) {
   next(createError(404));
 });
@@ -43,9 +59,14 @@ app.use(function (err, req, res) {
 // Set port
 const port = process.env.APP_PORT || 4000;
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Check env
+const env = process.env.ENV_TYPE || 'production';
+
+if (env === 'development') {
+  // Start server
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// Greeting API
+router.get('/', function (req, res) {
+  res.send('Selamat Datang\nDi Restful API Pemrogramana Web Guru Tamu');
+});
+
 module.exports = router;
